1c6331bc7197a93194c6511b30bc56068cff885d
Samuele Santi <redshadow@hackzine.org>
2013-04-18 16:19:34 +0200

    Added support for custom serialization methods

--- smartrpyc/tests/unit/test_serialization.py
+++ smartrpyc/tests/unit/test_serialization.py
@@ -6,20 +6,21 @@ import unittest
 
 
 class SerializationTestCase(unittest.TestCase):
-    def test_serialization(self):
-        from smartrpyc.utils.serialization import packb, unpackb
+    def _run_serialization_tests(self, packer, support_bytes=True):
         test_values = [
+            ## These should present no significant problems!
             'Hello, world!',
             ['hello', 'world'],
             {'Hello': 'world'},
             {'Hello': 'world', 'Hello2': 'world2'},
+            {'hello': ['w', 'o', 'r', 'l', 'd']},
+            True, False, None,
+            (2 ** 64) - 1,  # MAXINT (64bit) <-- Beware!
+            10, 1.0, 0.2, -100000, -10.5,
             u"This is a unicode string",
-            "\x01\x02\x03\x04\x05\x06",
             ['This is', u'a mixed', 'list'],
             u"Hélló wórld ¶¬¹²³¤€˛¡¿",
             {"this": u"is", "a": u"díctíöñáry"},
-            b"And this is \x00\x01\x02\x03 a py3k \x80\x81\x82\x83 binary",
-            u"\u1234\u5678 <-- this is... something!",
             {
                 'ok, we can even do this': [
                     u"Hello, world!",
@@ -32,18 +33,37 @@ class SerializationTestCase(unittest.TestCase):
                     10, 9, 8, 7, 6, 5, 5.5, 5.4, 5.3, 5.2, "Boom!", None,
                 ],
             },
-            True,
-            False,
-            None,
-            10,
-            (2 ** 64) - 1,  # MAXINT (64bit) <-- Beware!
-            1.0,
-            0.2,
-            -100000,
-            -10.5,
+            u"\u1234\u5678 <-- this is... something!",
+            u"What's this \x01 \x02 \x03 \x80\x81\x92\x93 crap??",
         ]
 
+        ## These should work only with msgpack_custom and pickle
+        extended_test_values = [
+            "\x01\x02\x03\x04\x05\x06",
+            b"And this is \x00\x01\x02\x03 a py3k \x80\x81\x82\x83 binary",
+        ]
+
+        if support_bytes:
+            test_values += extended_test_values
+
         for value in test_values:
-            _new_value = unpackb(packb(value))
+            _new_value = packer.unpackb(packer.packb(value))
             self.assertEqual(value, _new_value)
-            self.assertEqual(type(value), type(_new_value))
+            if support_bytes:
+                self.assertEqual(type(value), type(_new_value))
+
+    def test_serialization_msgpack(self):
+        from smartrpyc.utils.serialization import MsgPackSerializer
+        self._run_serialization_tests(MsgPackSerializer, support_bytes=False)
+
+    def test_serialization_msgpack_custom(self):
+        from smartrpyc.utils.serialization import CustomMsgPackSerializer
+        self._run_serialization_tests(CustomMsgPackSerializer)
+
+    def test_serialization_json(self):
+        from smartrpyc.utils.serialization import JsonSerializer
+        self._run_serialization_tests(JsonSerializer, support_bytes=False)
+
+    def test_serialization_pickle(self):
+        from smartrpyc.utils.serialization import PickleSerializer
+        self._run_serialization_tests(PickleSerializer)
--- smartrpyc/utils/serialization.py
+++ smartrpyc/utils/serialization.py
@@ -1,84 +1,132 @@
 """
 Serialization utilities
 """
+import json
+import pickle
 
-# from msgpack import Packer, Unpacker, pack
-import struct
-import msgpack.fallback
-from msgpack.exceptions import PackValueError, ExtraData
-from msgpack.fallback import DEFAULT_RECURSE_LIMIT, int_types, Unicode, \
-    dict_iteritems
+import msgpack
+from msgpack.fallback import Unicode
 
 
-def pack(o, stream, default=None, encoding='utf-8', unicode_errors='strict'):
-    """ Pack object `o` and write it to `stream` """
-    packer = Packer(default=default, encoding=encoding,
-                    unicode_errors=unicode_errors)
-    stream.write(packer.pack(o))
+__all__ = ['MsgPackSerializer', 'CustomMsgPackSerializer', 'JsonSerializer',
+           'PickleSerializer']
 
 
-def packb(o, default=None, encoding='utf-8', unicode_errors='struct',
-          use_single_float=False):
-    """ Pack object `o` and return packed bytes """
-    packer = Packer(default=default,
-                    encoding=encoding,
-                    unicode_errors=unicode_errors,
-                    use_single_float=use_single_float)
-    return packer.pack(o)
+class MsgPackSerializer(object):
+    @staticmethod
+    def packb(o):
+        return msgpack.packb(o, encoding='utf-8')
+
+    @staticmethod
+    def unpackb(packed):
+        return msgpack.unpackb(packed, encoding='utf-8')
 
 
-def unpack(stream, object_hook=None, list_hook=None, use_list=True,
-           encoding=None, unicode_errors='strict',
-           object_pairs_hook=None):
-    """ Unpack an object from `stream`.
+class CustomMsgPackSerializer(object):
+    """
+    Custom msgpack serializer, allowing to differentiate between
+    strings and blobs. It's way more inefficient thant the MsgPackSerializer,
+    however, since it has to use the fallback Python-only version..
+    """
+    @staticmethod
+    def packb(o):
+        return msgpack.packb(_pack_strings(o))
+        # packer = CustomPacker()
+        # return packer.pack(o)
 
-    Raises `ExtraData` when `stream` has extra bytes. """
-    unpacker = Unpacker(stream, object_hook=object_hook, list_hook=list_hook,
-                        use_list=use_list,
-                        encoding=encoding, unicode_errors=unicode_errors,
-                        object_pairs_hook=object_pairs_hook)
-    ret = unpacker._fb_unpack()
-    if unpacker._fb_got_extradata():
-        raise ExtraData(ret, unpacker._fb_get_extradata())
-    return ret
-
-
-def unpackb(packed, object_hook=None, list_hook=None, use_list=True,
-            encoding=None, unicode_errors='strict',
-            object_pairs_hook=None):
-    """ Unpack an object from `packed`.
-
-    Raises `ExtraData` when `packed` contains extra bytes. """
-    unpacker = Unpacker(None, object_hook=object_hook, list_hook=list_hook,
-                        use_list=use_list,
-                        encoding=encoding, unicode_errors=unicode_errors,
-                        object_pairs_hook=object_pairs_hook)
-    unpacker.feed(packed)
-    ret = unpacker._fb_unpack()
-    if unpacker._fb_got_extradata():
-        raise ExtraData(ret, unpacker._fb_get_extradata())
-    return ret
+    @staticmethod
+    def unpackb(packed):
+        return _unpack_strings(msgpack.unpackb(packed))
+        # unpacker = CustomUnpacker(None)
+        # unpacker.feed(packed)
+        # ret = unpacker._fb_unpack()
+        # if unpacker._fb_got_extradata():
+        #     raise ExtraData(ret, unpacker._fb_get_extradata())
+        # return ret
 
 
-class Packer(msgpack.fallback.Packer):
-    def _pack(self, obj, *args, **kwargs):
+def _pack_strings(o):
+    if isinstance(o, Unicode):
+        return "u" + o.encode('utf-8')
+    elif isinstance(o, bytes):
+        return "b" + o
+    elif isinstance(o, dict):
+        return dict(
+            (_pack_strings(k), _pack_strings(v))
+            for k, v in o.iteritems())
+    elif isinstance(o, (list, tuple)):
+        return [_pack_strings(x) for x in o]
+    else:
+        return o
 
-        if isinstance(obj, Unicode):
-            obj = "u" + obj.encode('utf-8')
 
-        elif isinstance(obj, bytes):
-            obj = "b" + obj
+def _unpack_strings(o):
+    if isinstance(o, bytes):
+        if o[0] == 'u':
+            return o[1:].decode('utf-8')
+        elif o[0] == 'b':
+            return o[1:]
+        raise ValueError("Invalid string object prefix {}!".format(o[0]))
+    elif isinstance(o, dict):
+        return dict(
+            (_unpack_strings(k), _unpack_strings(v))
+            for k, v in o.iteritems())
+    elif isinstance(o, (list, tuple)):
+        return [_unpack_strings(x) for x in o]
+    else:
+        return o
 
-        return super(Packer, self)._pack(obj, *args, **kwargs)
+
+# class CustomPacker(msgpack.fallback.Packer):
+#     def _pack(self, obj, *args, **kwargs):
+#
+#         if isinstance(obj, Unicode):
+#             obj = "u" + obj.encode('utf-8')
+#
+#         elif isinstance(obj, bytes):
+#             obj = "b" + obj
+#
+#         return super(CustomPacker, self)._pack(obj, *args, **kwargs)
+#
+#
+# class CustomUnpacker(msgpack.fallback.Unpacker):
+#     def _fb_unpack(self, *args, **kwargs):
+#         data = super(CustomUnpacker, self)._fb_unpack(*args, **kwargs)
+#         if isinstance(data, bytes):
+#             if data[0] == 'u':
+#                 return data[1:].decode('utf-8')
+#             elif data[0] == 'b':
+#                 return data[1:]
+#             raise ValueError("Invalid string object prefix {}!".format(data[0]))
+#         return data
 
 
-class Unpacker(msgpack.fallback.Unpacker):
-    def _fb_unpack(self, *args, **kwargs):
-        data = super(Unpacker, self)._fb_unpack(*args, **kwargs)
-        if isinstance(data, bytes):
-            if data[0] == 'u':
-                return data[1:].decode('utf-8')
-            elif data[0] == 'b':
-                return data[1:]
-            raise ValueError("Invalid string object prefix {}!".format(data[0]))
-        return data
+class JsonSerializer(object):
+    """
+    .. warning::
+        We have a problem with blobs here, since all the strings
+        are automatically converted to unicode...
+    """
+    @staticmethod
+    def packb(o):
+        return json.dumps(o)
+
+    @staticmethod
+    def unpackb(packed):
+        return json.loads(packed)
+
+
+class PickleSerializer(object):
+    """
+    Pickle-powered serializer
+
+    .. warning::
+        Never, ever, use this for untrusted data!! Big security risk!!
+    """
+    @staticmethod
+    def packb(o):
+        return pickle.dumps(o)
+
+    @staticmethod
+    def unpackb(packed):
+        return pickle.loads(packed)
