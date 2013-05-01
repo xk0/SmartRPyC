a51f8838c925d2e6731c3377b4a38f6d05a3cc6a
Samuele Santi <redshadow@hackzine.org>
2013-05-01 19:10:30 +0200

    More 2to3 conversion functionality, using script

--- tools/run-2to3.sh
+++ tools/run-2to3.sh
@@ -1,1 +1,9 @@
-2to3 -W -o ../smartrpyc-py3k/ -n .
+#!/bin/bash
+
+ROOTDIR="$( dirname "$BASH_SOURCE" )/.."
+cd "$ROOTDIR"
+
+DESTINATION="../smartrpyc-py3k"
+
+2to3 -W -o "$DESTINATION"/ -n .
+cp -t "$DESTINATION" .gitignore README.rst LICENSE MANIFEST.in
