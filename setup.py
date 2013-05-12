af637fe5aa87a0357af4e4d4f3d11be24840286e
Samuele Santi <redshadow@hackzine.org>
2013-05-12 22:26:49 +0200

    Tests up&running with py3k too
    * ``setup.py test`` up & running
    * ``tox`` up & runing
    * So not use py.test directly on the source with py3k, of course!
    * Now testing on TravisCI..

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 ## SmartRPyC setup.py
 
 import sys
+from pkg_resources import normalize_path
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
+
 
 version = __import__('smartrpyc').__version__
 if 'smartrpyc' in sys.modules:
@@ -22,10 +24,6 @@ if sys.version_info >= (3,):
     #extra['convert_2to3_doctests'] = ['src/your/module/README.txt']
     #extra['use_2to3_fixers'] = ['your.fixers']
 
-# if sys.version_info <= (2, 7):
-#     ## We need unittest2 for Python < 2.7
-#     tests_require.append('unittest2')
-
 
 class PyTest(TestCommand):
     def finalize_options(self):
@@ -34,9 +32,31 @@ class PyTest(TestCommand):
         self.test_suite = True
 
     def run_tests(self):
+        from pkg_resources import _namespace_packages
         import pytest
+
+        # Purge modules under test from sys.modules. The test loader will
+        # re-import them from the build location. Required when 2to3 is used
+        # with namespace packages.
+        if sys.version_info >= (3,) and getattr(self.distribution, 'use_2to3', False):
+            module = self.test_args[-1].split('.')[0]
+            if module in _namespace_packages:
+                del_modules = []
+                if module in sys.modules:
+                    del_modules.append(module)
+                module += '.'
+                for name in sys.modules:
+                    if name.startswith(module):
+                        del_modules.append(name)
+                map(sys.modules.__delitem__, del_modules)
+
+            ## Run on the build directory for 2to3-built code..
+            ei_cmd = self.get_finalized_command("egg_info")
+            self.test_args = [normalize_path(ei_cmd.egg_base)]
+
         errno = pytest.main(self.test_args)
         sys.exit(errno)
+
 
 setup(
     name='SmartRPyC',
