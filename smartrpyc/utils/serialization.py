2cc511374e716511f612c1f8943a809af81ff1a7
Samuele Santi <redshadow@hackzine.org>
2013-11-24 20:57:10 +0100

    Removed CustomMsgpackSerializer (fixes #1)
    The new msgpack supports the difference between bytes and unicode strings,
    so we can get rid of that ugly hack. Yay!

--- smartrpyc/utils/serialization.py
+++ smartrpyc/utils/serialization.py
@@ -5,7 +5,6 @@ import json
 import pickle
 
 import msgpack
-from msgpack.fallback import Unicode
 
 
 __all__ = ['MsgPackSerializer', 'CustomMsgPackSerializer', 'JsonSerializer',
@@ -13,60 +12,30 @@ __all__ = ['MsgPackSerializer', 'CustomMsgPackSerializer', 'JsonSerializer',
 
 
 class MsgPackSerializer(object):
+    """
+    Messagepack-based serializer.
+    Thanks to the new features added in msgpack, we now support
+    binary vs unicode strings natively, and other nice things.
+    """
+
     @staticmethod
     def packb(o):
-        return msgpack.packb(o, encoding='utf-8')
+        #return msgpack.packb(o, encoding='utf-8')
+        msgpack_packer = msgpack.Packer(encoding='utf-8', use_bin_type=True)
+        return msgpack_packer.pack(o)
 
     @staticmethod
     def unpackb(packed):
-        return msgpack.unpackb(packed, encoding='utf-8')
+        #return msgpack.unpackb(packed, encoding='utf-8')
+        msgpack_unpacker = msgpack.Unpacker(encoding='utf-8')
+        msgpack_unpacker.feed(packed)
+        return msgpack_unpacker.unpack()
 
 
-class CustomMsgPackSerializer(object):
-    """
-    Custom msgpack serializer, allowing to differentiate between
-    strings and blobs. It's way more inefficient thant the MsgPackSerializer,
-    however, since it has to use the fallback Python-only version..
-    """
-    @staticmethod
-    def packb(o):
-        return msgpack.packb(_pack_strings(o))
-
-    @staticmethod
-    def unpackb(packed):
-        return _unpack_strings(msgpack.unpackb(packed))
-
-
-def _pack_strings(o):
-    if isinstance(o, Unicode):
-        return b"u" + o.encode('utf-8')
-    elif isinstance(o, bytes):
-        return b"b" + o
-    elif isinstance(o, dict):
-        return dict(
-            (_pack_strings(k), _pack_strings(v))
-            for k, v in o.iteritems())
-    elif isinstance(o, (list, tuple)):
-        return [_pack_strings(x) for x in o]
-    else:
-        return o
-
-
-def _unpack_strings(o):
-    if isinstance(o, bytes):
-        if o[0] in (b'u', ord(b'u')):
-            return o[1:].decode('utf-8')
-        elif o[0] in (b'b', ord(b'b')):
-            return o[1:]
-        raise ValueError("Invalid string object prefix {0}!".format(o[0]))
-    elif isinstance(o, dict):
-        return dict(
-            (_unpack_strings(k), _unpack_strings(v))
-            for k, v in o.iteritems())
-    elif isinstance(o, (list, tuple)):
-        return [_unpack_strings(x) for x in o]
-    else:
-        return o
+## For backwards compatibility.
+## This is now deprecated in favor of standard
+## MsgPackSerializer.
+CustomMsgPackSerializer = MsgPackSerializer
 
 
 class JsonSerializer(object):
